0089b15fd0ef843441459bef819603205040fcc7
Hazim Alhaidari <hazimhamid5@gmail.com>
2021-09-27 08:27:00 +0500

    check point 1

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//
+// 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
@@ -7,30 +7,33 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 
-//routes requiring
+//routes requiring -- (from routes folder)
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
-// templating engine --- views
+// template engine --- views
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 
-//layouts
+//layout
 const expressLayouts = require('express-ejs-layouts')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 
-// public
+// public (static files)
 app.use(express.static('public'))
 
-// database
+// database -- MongoDB
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
+
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected'))
 
-// routes using
+// routes using (that we imported from the routes file)
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 // listening
 app.listen(process.env.PORT || 3000)
